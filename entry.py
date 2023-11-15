e5ab426600de1a772ef98ae81ddd010e7f8ef01c
BuildTools <unconfigured@null.spigotmc.org>
2023-11-14 19:07:30 -0800

    Added ban command

--- entry.py
+++ entry.py
@@ -10,15 +10,16 @@ client = commands.Bot(os.getenv('command_prefix'), intents=discord.Intents.all()
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
-    print(os.getenv('command_prefix'))
+    print(f"The current prefix is {os.getenv('command_prefix')}")
     await client.change_presence(activity=discord.CustomActivity(name="Rotating Ominously"))
 
-@client.command('tyler')
-async def ping(ctx):
-    await ctx.send("im coming")
-
-@client.command('sex')
-async def sex(ctx):
-    await ctx.send("I will...")
+@client.command('ban')
+@commands.has_permissions(ban_members = True)
+async def bannywanny(ctx, member : discord.Member, reason = None):
+    if reason == None:
+        await ctx.send("Provide a reason")
+    else:
+        await ctx.send(f"Alright, {member.name} was banned!")
+        await member.ban(reason=reason)
 
 client.run(os.getenv('bot_token'))
